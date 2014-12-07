da423b2458b7ef1438663c08c8708451673e8abf
Bradford Knowlton <brad@ucanbonline.net>
2014-12-06 19:54:52 -0700

    Version Bump

--- dm-frequently-asked-questions.php
+++ dm-frequently-asked-questions.php
@@ -5,7 +5,7 @@ Plugin Name: Frequently Asked Questions
 Plugin URI: http://www.designmissoula.com/
 Description: Used by Millions to make WordPress better.
 Author: Bradford Knowlton
-Version: 2.0.1
+Version: 2.1.1
 Author URI: http://bradknowlton.com/
 GitHub Plugin URI: https://github.com/DesignMissoula/DM-frequently-asked-questions
 GitHub Branch: master
