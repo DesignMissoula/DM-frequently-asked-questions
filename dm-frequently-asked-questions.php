a079770ae9e1788d0283f1903a845255afbb1931
Bradford Knowlton <brad@ucanbonline.net>
2014-12-06 19:48:31 -0700

    Initial Commit

---
+++ dm-frequently-asked-questions.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Frequently Asked Questions
+Plugin URI: http://www.designmissoula.com/
+Description: Used by Millions to make WordPress better.
+Author: Bradford Knowlton
+Version: 1.9.9
+Author URI: http://bradknowlton.com/
+GitHub Plugin URI: https://github.com/DesignMissoula/DM-frequently-asked-questions
+GitHub Branch: master
+*/
+
